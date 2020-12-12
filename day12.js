4b1663359a0cc8652000b8e3caa96b863e6f34de
Bikalpa <bikalpa.thapa@yipl.com.np>
2020-12-12 18:15:44 +0545

    Day12 Part 2 Solution

--- day12.js
+++ day12.js
@@ -52,5 +52,69 @@ const manhattanDistance = (data) => {
   }
   return obj["E"] + obj["S"];
 };
+
 //Part 1 Solution
 console.log(manhattanDistance(data));
+
+//Part 2 Solution
+waypoint = {
+  E: 10,
+  N: 1,
+  S: 0,
+  W: 0,
+};
+rotateWaypoint = (waypoint, degree, clockwise) => {
+  newWayPoint = {};
+  for (key in waypoint) {
+    if (waypoint[key]) {
+      newKey = calculateRotation(key, degree, clockwise);
+      newWayPoint[newKey] = waypoint[key];
+    }
+  }
+  return newWayPoint;
+};
+
+const manhattanDistance2 = (data) => {
+  obj = { E: 0, S: 0, W: 0, N: 0 };
+  for (i = 0; i < data.length; i++) {
+    [x, y] = data[i].split(/(\d+)/);
+    moveUnits = +y;
+    switch (x) {
+      case "F":
+        for (key in waypoint) {
+          if (waypoint[key]) {
+            if (key == "N") {
+              obj["N"] += waypoint[key] * moveUnits;
+            } else if (key == "S") {
+              obj["S"] += waypoint[key] * moveUnits;
+            } else if (key == "W") {
+              obj["W"] += waypoint[key] * moveUnits;
+            } else {
+              obj["E"] += waypoint[key] * moveUnits;
+            }
+          }
+        }
+        break;
+      case "N":
+        waypoint["N"] += moveUnits;
+        break;
+      case "E":
+        waypoint["E"] += moveUnits;
+        break;
+      case "W":
+        waypoint["W"] += moveUnits;
+        break;
+      case "S":
+        waypoint["S"] += moveUnits;
+        break;
+      case "R":
+        waypoint = rotateWaypoint(waypoint, moveUnits);
+        break;
+      case "L":
+        waypoint = rotateWaypoint(waypoint, moveUnits, false);
+        break;
+    }
+  }
+  return obj.E - obj.W + obj.N - obj.S;
+};
+console.log(manhattanDistance2(data));
