355575ce9cee77c95a03bccbe47f8962d3d5972d
Bikalpa <bikalpa.thapa@yipl.com.np>
2020-12-15 09:21:27 +0545

    Day14 Part 2 Solution

--- day14.js
+++ day14.js
@@ -3,14 +3,16 @@ lines = fs.readFileSync("./input14.txt", "utf8");
 data = lines.split("\n");
 data.pop();
 
-const getAllIndexesOf0and1 = (str) => {
+const getAllIndexesOf0X1 = (str) => {
   let zeros = [],
-    ones = [];
+    ones = [],
+    xs = [];
   for (i = 0; i < str.length; i++) {
     if (str[i] == 0) zeros.push(i);
     if (str[i] == 1) ones.push(i);
+    if (str[i] == "X") xs.push(i);
   }
-  return [zeros, ones];
+  return [zeros, ones, xs];
 };
 
 const allIndexin = (arrOfIndices, str, val) =>
@@ -24,14 +26,19 @@ const replaceStringIn = (str, indices, val) => {
   return arr.join("");
 };
 
+const binaryWithPadding0 = (num, padLen) =>
+  String((+num).toString(2)).padStart(padLen, 0);
+
+const sumOfValues = (obj) => Object.values(obj).reduce((x, y) => x + y);
+
 const sumOfAll = (data) => {
   obj = {};
   for (inp = 0; inp < data.length; inp++) {
-      [mem, val] = data[inp].split(" = ");
-    if (mem == "mask") [zeros, ones] = getAllIndexesOf0and1(val);
+    [mem, val] = data[inp].split(" = ");
+    if (mem == "mask") [zeros, ones, _] = getAllIndexesOf0X1(val);
     else {
       key = mem.match(/\d+/)[0];
-      binary = String((+val).toString(2)).padStart(36, 0);
+      binary = binaryWithPadding0(val, 36);
       if (allIndexin(zeros, binary, "0") && allIndexin(ones, binary, "1"))
         obj[key] = +val;
       else
@@ -41,8 +48,49 @@ const sumOfAll = (data) => {
     }
   }
 
-  return Object.values(obj).reduce((num1, num2) => num1 + num2);
+  return sumOfValues(obj);
 };
 
 //Part 1 Solution
 console.log(sumOfAll(data));
+
+const replaceWithX = (str, indices) => {
+  indicesLen = indices.length;
+  len = Math.pow(2, indicesLen);
+  arr = [];
+  for (let ind = 0; ind < len; ind++) {
+    arr.push(binaryWithPadding0(ind, indicesLen));
+  }
+
+  arrLen = arr.length;
+  valArr = [];
+  for (let j = 0; j < arrLen; j++) {
+    strArr = str.split("");
+    for (let k = 0; k < indicesLen; k++) {
+      strArr[indices[k]] = arr[j][k];
+    }
+    valArr.push(+("0b" + strArr.join("")));
+  }
+
+  return valArr;
+};
+
+const sumOfAll2 = (data) => {
+  newObj = {};
+  for (inp = 0; inp < data.length; inp++) {
+    [mem, val] = data[inp].split(" = ");
+    if (mem == "mask") [_, ones, xs] = getAllIndexesOf0X1(val);
+    else {
+      key = mem.match(/\d+/)[0];
+      bin = binaryWithPadding0(key, 36);
+      memAddressToWrite = replaceWithX(replaceStringIn(bin, ones, 1), xs);
+      memAddressToWrite.forEach((addr) => {
+        newObj[addr] = +val;
+      });
+    }
+  }
+  return sumOfValues(newObj);
+};
+
+//Part 2 Solution
+console.log(sumOfAll2(data));
