44c1f31dab9d677104d5792ca0e267ea307c0001
Bikalpa <bikalpa.thapa@yipl.com.np>
2020-12-23 19:12:23 +0545

    Day22 Part 2 Solution

--- day22.js
+++ day22.js
@@ -13,12 +13,47 @@ const playCards = (arr1, arr2) => {
     if (first > second) arr1.push(first, second);
     else arr2.push(second, first);
   }
-  if (arr1.length) {
-    return arr1.reverse();
-  }
+
+  if (arr1.length) return arr1.reverse();
   return arr2.reverse();
 };
 
+// Part 1 Solution
 console.log(
-  playCards(p1Nums, p2Nums).reduce((acc, val, idx) => acc + val * (idx + 1), 0)
+  playCards([...p1Nums], [...p2Nums]).reduce(
+    (acc, val, idx) => acc + val * (idx + 1),
+    0
+  )
+);
+
+// Part 2 Solution
+const recurse = (arr1, arr2) => {
+  const visitedNums = new Set();
+  while (arr1.length && arr2.length) {
+    hash = [...arr1, " ", ...arr2].join("");
+    if (visitedNums.has(hash)) return { arr: arr1, winner: 1 };
+    visitedNums.add(hash);
+
+    const first = arr1.shift();
+    const second = arr2.shift();
+    if (first <= arr1.length && second <= arr2.length) {
+      newArr1 = arr1.slice(0, first);
+      newArr2 = arr2.slice(0, second);
+      const { winner } = recurse(newArr1, newArr2);
+      if (winner == 1) arr1.push(first, second);
+      else arr2.push(second, first);
+    } else {
+      if (first > second) arr1.push(first, second);
+      else arr2.push(second, first);
+    }
+  }
+
+  if (arr1.length) return { arr: arr1, winner: 1 };
+  return { arr: arr2, winner: 2 };
+};
+
+console.log(
+  recurse(p1Nums, p2Nums)
+    ["arr"].reverse()
+    .reduce((acc, val, idx) => acc + val * (idx + 1), 0)
 );
